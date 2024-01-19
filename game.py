4f709c4e9406429936a6e14db76bb484508d57ee
Eger_tech <133259370+newBest123-rgb@users.noreply.github.com>
2024-01-19 18:14:21 +0300

    Update game.py

--- game.py
+++ game.py
@@ -3,48 +3,49 @@ import pygame_menu
 import sys
 import random
 
+# инициализируем модуль pygame, для обработки воспроизведения звука
 pygame.mixer.pre_init(44100, -16, 1, 512)
 
 bg_image = pygame.image.load("logo_snake.png")
 WHITE = pygame.Color("white")
 RED = pygame.Color("red")
-SIZE_BLOCK = 20
-COUNT_BLOCKS = 20
+SIZE_BLOCK = 20  # размер каждого блока
+COUNT_BLOCKS = 20  # кол-во блоков
+
+# Цвета для клеток на поле
 BLUE = (204, 255, 255)
 HEADER_COLOR = (0, 204, 153)
+
 SNAKE_COLOR = (0, 102, 0)
 MARGIN = 1
 HEADER_MARGIN = 70
 FRAME_COLOR = (0, 255, 204)
-snake_block = [[1, 2], [3, 4]]
+snake_block = [
+    [1, 2],
+    [3, 4],
+]  # список содержит два списка, представляющие начальные позиции блоков snake
 
 if __name__ == "__main__":
     pygame.init()
 
-    size = (
-        SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * COUNT_BLOCKS,
-        SIZE_BLOCK * COUNT_BLOCKS
-        + 2 * SIZE_BLOCK
-        + MARGIN * SIZE_BLOCK
-        + HEADER_MARGIN,
-    )
-    print(size)
+    size = (460, 530)
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Питон")
-    timer = pygame.time.Clock()
-    TNR = pygame.font.SysFont("Times New Roman", 36)
+    timer = pygame.time.Clock()  # таймер для управления частотой кадров
+    TNR = pygame.font.SysFont("Times New Roman", 36)  # шрифт
 
-    pygame.mixer.music.load("Snake Music.mp3")
+    pygame.mixer.music.load("Snake Music.mp3")  # фоновая музыка
     pygame.mixer.music.play(-1)
 
-    Apple_Music = pygame.mixer.Sound("Apple Music.mp3")
-    Game_Over_Music = pygame.mixer.Sound("Game_Over Music.mp3")
+    Apple_Music = pygame.mixer.Sound("Apple Music.mp3")  # музыка поедания яблока
+    Game_Over_Music = pygame.mixer.Sound("Game_Over Music.mp3")  # музыка проигрыша
 
     class SnakeBlock:
         def __init__(self, x, y):
             self.x = x
             self.y = y
 
+        # Функция для проверки, не вышла ли змейка за поля
         def is_inside(self):
             return 0 <= self.x < COUNT_BLOCKS and 0 <= self.y < COUNT_BLOCKS
 
@@ -55,6 +56,7 @@ if __name__ == "__main__":
                 and self.y == other.y
             )
 
+    # рисуем блок
     def draw_block(color, row, column):
         pygame.draw.rect(
             screen,
@@ -72,6 +74,7 @@ if __name__ == "__main__":
             file.write(f"Имя: {names}, Результат: {total}\n")
 
     def start_the_game():
+        # Функция с помощью которой яблоко появляется на поле постоянно в рандомном месте
         def get_random_empty_block():
             x = random.randint(0, COUNT_BLOCKS - 1)
             y = random.randint(0, COUNT_BLOCKS - 1)
@@ -81,21 +84,25 @@ if __name__ == "__main__":
                 empty_block.y = random.randint(0, COUNT_BLOCKS - 1)
             return empty_block
 
-        snake_blocks = [SnakeBlock(9, 10)]
+        snake_blocks = [SnakeBlock(9, 10)]  # Голова змейки
         apple = get_random_empty_block()
+
+        # переменные для считывания событий на клавиатуре
         d_row = buf_row = 0
         d_col = buf_col = 1
-        total = 0
-        speed = 1
+        total = 0  # результат змейки
+        speed = 1  # скорость змейки
 
         running = True
 
         while running:
             for even in pygame.event.get():
                 if even.type == pygame.QUIT:
-                    print("exit")
+                    print("Выход")
                     pygame.quit()
                     sys.exit()
+
+                # Считываем события с клавиатуры
                 elif even.type == pygame.KEYDOWN:
                     if even.key == pygame.K_UP and d_col != 0:
                         buf_row = -1
@@ -109,15 +116,19 @@ if __name__ == "__main__":
                     elif even.key == pygame.K_LEFT and d_row != 0:
                         buf_row = 0
                         buf_col = -1
+            d_row = buf_row
+            d_col = buf_col
 
             screen.fill(FRAME_COLOR)
             pygame.draw.rect(screen, HEADER_COLOR, [0, 0, size[0], HEADER_MARGIN])
 
+            # Выводим на экран результат и скорость змейки
             text_total = TNR.render(f"Счёт: {total}", True, WHITE)
             text_speed = TNR.render(f"Скорость: {speed}", True, WHITE)
             screen.blit(text_total, (SIZE_BLOCK, SIZE_BLOCK))
             screen.blit(text_speed, (SIZE_BLOCK + 200, SIZE_BLOCK))
 
+            # Рисуем квадратики на поле
             for row in range(COUNT_BLOCKS):
                 for column in range(COUNT_BLOCKS):
                     if (row + column) % 2 == 0:
@@ -127,18 +138,21 @@ if __name__ == "__main__":
 
                     draw_block(color, row, column)
 
-            head = snake_blocks[-1]
+            head = snake_blocks[-1]  # Назначаем голову змейки
+            # Проверка на выход змейки за поле
             if not head.is_inside():
                 Game_Over_Music.play()
-                print("crash")
+                print("Вышел за поле")
                 break
 
+            # назначаем первоначальное направление змейки (вправо)
             draw_block(RED, apple.x, apple.y)
             for block in snake_blocks:
                 draw_block(SNAKE_COLOR, block.x, block.y)
 
             pygame.display.flip()
 
+            # проверка на то, чтобы яблоко не заспавнилось на том пикселе, где находится часть змейки
             if apple == head:
                 total += 1
                 speed = total // 5 + 1
@@ -146,8 +160,6 @@ if __name__ == "__main__":
                 apple = get_random_empty_block()
                 Apple_Music.play()
 
-            d_row = buf_row
-            d_col = buf_col
             new_head = SnakeBlock(head.x + d_row, head.y + d_col)
 
             if new_head in snake_blocks:
@@ -158,10 +170,11 @@ if __name__ == "__main__":
             snake_blocks.append(new_head)
             snake_blocks.pop(0)
 
-            timer.tick(3 + speed)
+            timer.tick(3 + speed)  # Увеличение скорости змейки
 
-        save_score_to_file(name.get_value(), total)
+        save_score_to_file(name.get_value(), total)  # аргументы для сохранения в .txt
 
+    # Создаем меню
     main_theme = pygame_menu.themes.THEME_DARK.copy()
     main_theme.set_background_color_opacity(0.6)  # Прозрачность меню
 
@@ -179,8 +192,8 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 exit()
 
-        if menu.is_enabled():
+        if menu.is_enabled():  # проверяем включено ли меню
             menu.update(events)
-            menu.draw(screen)
+            menu.draw(screen)  # рисуем меню
 
         pygame.display.update()
